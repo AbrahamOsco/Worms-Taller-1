ce01a618839cc2e882a5bec2460c669682a579d6
Abraham Osco <aosco@fi.uba.ar>
2023-09-05 12:22:17 -0300

    Se elimina codigo, se ordena en directorios

--- Modelo/main.cpp
+++ Modelo/main.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
 
 #include <SDL2/SDL.h>
-#include "src/core/Engine.h"
-#include "src/Graphics/TextManager.h"
+
 
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
 
-    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-        std::cout << "SDL fue inicializado con exito\n";
-    }
-    TextManager textManager;
-    Engine unEngine(textManager);
-    while (unEngine.isRunning()){
-        unEngine.events();
-        unEngine.update();
-        unEngine.render();
-    }
     return 0;
 }
 
