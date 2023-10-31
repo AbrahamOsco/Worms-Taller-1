5ea601ce1c05921a5c59adda0f68463317ceb202
anita <avernieri@fi.uba.ar>
2023-10-31 19:53:33 -0300

    sigo con el editor

--- Editor/include/editingWindow.h
+++ Editor/include/editingWindow.h
@@ -2,6 +2,8 @@
 #define EDITINGWINDOW_H
 
 #include <QWidget>
+#include  <QGraphicsRectItem>
+#include <vector>
 
 namespace Ui {
 class EditingWindow;
@@ -14,6 +16,16 @@ class EditingWindow : public QWidget
 private slots:
     void onGoBackBtnClicked();
 
+    void on_addWormBtn_clicked();
+
+    void on_addBeamBtn_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 public:
     explicit EditingWindow(QWidget *parent = nullptr);
     void setPrev(QWidget* prev);
@@ -23,6 +35,11 @@ public:
 private:
     Ui::EditingWindow *ui;
     QWidget *prev;
+    QGraphicsScene *scene;
+    std::vector<QGraphicsRectItem*> worms;
+    std::vector<QGraphicsRectItem*> beams;
+    int newBeamLength;
+    QGraphicsRectItem* limits;
 };
 
 #endif // EDITINGWINDOW_H
--- Editor/src/editingWindow.cpp
+++ Editor/src/editingWindow.cpp
@@ -3,9 +3,22 @@
 
 EditingWindow::EditingWindow(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::EditingWindow)
+    ui(new Ui::EditingWindow),
+    scene(nullptr),
+    newBeamLength(60)
+
 {
     ui->setupUi(this);
+    this->setAttribute(Qt::WA_DeleteOnClose);
+    scene  =  new QGraphicsScene;
+    ui->graphicsView->setScene(scene);
+    limits = scene->addRect(0,0,500,300,QPen(Qt::red));
+    ui->spinBox->setRange(0,180);
+    ui->spinBox->setSingleStep(10);
+    ui->heightValueBox->setRange(1,999);
+    ui->widthValueBox->setRange(1,999);
+    ui->heightValueBox->setValue(300);
+    ui->widthValueBox->setValue(500);
 }
 
 void EditingWindow::setPrev(QWidget *prev)
@@ -29,4 +42,58 @@ void EditingWindow::onGoBackBtnClicked()
 EditingWindow::~EditingWindow()
 {
     delete ui;
+    for (auto & worm : worms) {
+        delete worm;
+    }
+    for (auto & beam : beams) {
+        delete beam;
+    }
 }
+
+void EditingWindow::on_addWormBtn_clicked()
+{
+    QGraphicsRectItem* worm = scene->addRect(0,0,10,10);
+    worm->setFlag(QGraphicsItem::ItemIsMovable);
+    worm->setFlag(QGraphicsItem::ItemIsSelectable);
+    worms.push_back(worm);
+}
+
+
+void EditingWindow::on_addBeamBtn_clicked()
+{
+    QGraphicsRectItem* beam = scene->addRect(0,0,newBeamLength,8);
+    beam->setFlag(QGraphicsItem::ItemIsMovable);
+    beam->setFlag(QGraphicsItem::ItemIsSelectable);
+    beam->setRotation(ui->spinBox->value());
+    beams.push_back(beam);
+}
+
+
+void EditingWindow::on_pushButton_2_clicked()
+{
+    if (ui->pushButton_2->text() == "long") {
+        ui->pushButton_2->setText("short");
+        newBeamLength  = 30;
+    } else if (ui->pushButton_2->text() == "short") {
+        ui->pushButton_2->setText("long");
+        newBeamLength  = 60;
+    }
+}
+
+
+void EditingWindow::on_pushButton_3_clicked()
+{
+    auto list  = scene->selectedItems();
+    for(int i = 0; i<list.count(); i++){
+        scene->removeItem(list[i]);
+    }
+}
+
+
+void EditingWindow::on_pushButton_4_clicked()
+{
+    scene->removeItem(limits);
+    QGraphicsRectItem* newLimits = scene->addRect(limits->pos().x(),limits->pos().y(),ui->widthValueBox->value(),ui->heightValueBox->value(),QPen(Qt::red));
+    limits = newLimits;
+}
+
