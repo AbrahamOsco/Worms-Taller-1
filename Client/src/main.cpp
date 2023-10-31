43e33badfe7121b734777d3fb89dd04522e6f60f
Abraham Osco <aosco@fi.uba.ar>
2023-10-31 15:54:00 -0300

    Se une SDL + QT en un solo add_executable con ayuda de @Ricardo 1) Se necesita un metodo de qt que devuleva una referencia al socket.

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,16 +1,19 @@
+#include <qt5/QtWidgets/QApplication>
 #include "protocol/ClientProtocol.h"
 #include "game/Game.h"
+#include "login.h"
 
-int main () {
-    const char* ip = "127.0.0.1";
-    const char* port = "8080";
+int main (int argc, char *argv[]) {
+    // Clase que contiene el loop principal
+    QApplication app(argc, argv);
+    // Instancio el greeter
+    Login login;
+    login.show();
+    // Arranca el loop de la UI
+    app.exec();
 
-    Socket skt(ip, port);
-    /*
-     * Aca va el looby de QT
-     */
-    Game game(skt);
-    game.loadMap();
-    game.run();
+    //Game game(skt);
+    //game.loadMap();
+    //game.run();
     return 0;
 }
