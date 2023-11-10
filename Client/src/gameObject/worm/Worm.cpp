0e115c416833d576d91bd5107574065828719d2d
rluizaga <rluizaga@fi.uba.ar>
2023-11-10 18:43:38 -0300

    se modifica posicion de la vida del worm

--- Client/src/gameObject/worm/Worm.cpp
+++ Client/src/gameObject/worm/Worm.cpp
@@ -15,7 +15,16 @@ void Worm::draw(SDL2pp::Renderer &renderer, TextureManager &textureManager) {
     SDL_Color textColor = {225, 225, 225, 255};
     SDL_Color boxColor = {0, 0, 0, 255};
     std::string fontPath = "../Client/resources/fonts/GROBOLD.ttf";
-    textureManager.drawTextBox(std::to_string(m_hpWorm), m_x - 20, m_y - 30, fontPath, 11, textColor, boxColor,
+
+    std::string text = std::to_string(m_hpWorm);
+    int fontSize = 11;
+
+    SDL2pp::Font font(fontPath, fontSize);
+    SDL2pp::Texture textTexture(renderer, font.RenderText_Blended(text, textColor));
+
+    int textWidth = textTexture.GetWidth();
+
+    textureManager.drawTextBox(text, m_x - textWidth/2, m_y - 30, fontPath, fontSize, textColor, boxColor,
                                renderer);
     m_animation.draw(m_x - m_width / 2, m_y - m_height / 2, m_flip, renderer, textureManager);
 }
