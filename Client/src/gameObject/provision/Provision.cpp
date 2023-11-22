4452aaef7d697e0d90900b93a72832ef64c9cba4
rluizaga <rluizaga@fi.uba.ar>
2023-11-22 12:54:42 -0300

    se agrega sonido al explotar la caja

--- Client/src/gameObject/provision/Provision.cpp
+++ Client/src/gameObject/provision/Provision.cpp
@@ -11,3 +11,10 @@ void Provision::draw(SDL2pp::Renderer &renderer, TextureManager &textureManager,
     int yCorrection = m_y - m_height / 2 - camera.getPosition().GetY();
     textureManager.draw(m_textureID, xCorrection, yCorrection, m_width, m_height, renderer);
 }
+
+void
+Provision::update(Input &input, Queue<std::unique_ptr<Command>> &queue, Camera &camera, SoundManager &soundManager) {
+    if (m_typeEffect == TypeEffect::EXPLOSION) {
+        soundManager.playEffect("explosion");
+    }
+}
--- Client/src/gameObject/provision/Provision.h
+++ Client/src/gameObject/provision/Provision.h
@@ -16,7 +16,7 @@ public:
     void draw(SDL2pp::Renderer &renderer, TextureManager &textureManager, Camera &camera) override;
 
     void
-    update(Input &input, Queue<std::unique_ptr<Command>> &queue, Camera &camera, SoundManager &soundManager) override{}
+    update(Input &input, Queue<std::unique_ptr<Command>> &queue, Camera &camera, SoundManager &soundManager) override;
 
 };
 
