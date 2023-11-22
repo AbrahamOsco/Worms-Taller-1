9a23a7fe5c0be65905c8fcbdc2d9bb4829b6eefd
rluizaga <rluizaga@fi.uba.ar>
2023-11-22 19:59:21 -0300

    se agregan mas sonidos e imagenes a los provisiones

--- Client/src/gameObject/provision/Provision.cpp
+++ Client/src/gameObject/provision/Provision.cpp
@@ -11,15 +11,27 @@ void Provision::draw(SDL2pp::Renderer &renderer, TextureManager &textureManager,
     int yCorrection = m_y - m_height / 2 - camera.getPosition().GetY();
     if (m_typeContact == TypeContact::NO_CONTACT ) {
         textureManager.draw(m_textureID, xCorrection, yCorrection, m_width, m_height, renderer);
-    } else if (m_typeContact == TypeContact::CONTACT && m_typeEffect == TypeEffect::EXPLOSION) {
-        textureManager.drawFrame("explosion", xCorrection, yCorrection, 60, 60, 0, 0,renderer,SDL_FLIP_NONE);
+    } else if (m_typeContact == TypeContact::CONTACT) {
+        if (m_typeEffect == TypeEffect::EXPLOSION) {
+            textureManager.drawFrame("explosion", xCorrection, yCorrection, 60, 60, 0, 0,renderer,SDL_FLIP_NONE);
+        } else if (m_typeEffect == TypeEffect::MEDICAL_KIT) {
+            textureManager.drawFrame("medicine", xCorrection, yCorrection, 60, 60, 0, 0,renderer,SDL_FLIP_NONE);
+        } else if (m_typeEffect == TypeEffect::MUNITIONS) {
+            textureManager.drawFrame("munition", xCorrection, yCorrection, 60, 60, 0, 0,renderer,SDL_FLIP_NONE);
+        }
     }
 
 }
 
 void
 Provision::update(Input &input, Queue<std::unique_ptr<Command>> &queue, Camera &camera, SoundManager &soundManager) {
-    if (m_typeContact == TypeContact::CONTACT_SOUND && m_typeEffect == TypeEffect::EXPLOSION) {
-        soundManager.playEffect("explosion");
+    if (m_typeContact == TypeContact::CONTACT_SOUND) {
+        if (m_typeEffect == TypeEffect::EXPLOSION) {
+            soundManager.playEffect("explosion");
+        } else if (m_typeEffect == TypeEffect::MEDICAL_KIT) {
+            soundManager.playEffect("collect_medicine");
+        }else if (m_typeEffect == TypeEffect::MUNITIONS) {
+            soundManager.playEffect("collect_weapon");
+        }
     }
 }
