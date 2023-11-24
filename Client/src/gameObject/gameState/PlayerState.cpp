4e8f13ffb9410fc978334705d03110e9c33ff769
rluizaga <rluizaga@fi.uba.ar>
2023-11-24 19:43:19 -0300

    se arregla imagenes de victoria y derrota

--- Client/src/gameObject/gameState/PlayerState.cpp
+++ Client/src/gameObject/gameState/PlayerState.cpp
@@ -9,9 +9,9 @@ PlayerState::PlayerState(const GameState &gameState, const TypeResult &typeResul
 void PlayerState::draw(SDL2pp::Renderer &renderer, TextureManager &textureManager, Camera &camera) {
     if (m_gameState == GameState::GAME_END) {
         if (m_typeResult == TypeResult::WON_THE_GAME) {
-            textureManager.draw("victory", 0, 0, 1876, 1208, renderer);
+            textureManager.draw("victory", 0, 0, 1024, 768, renderer);
         } else {
-            textureManager.draw("defeat", 0, 0, 1800, 1142, renderer);
+            textureManager.draw("defeat", 0, 0, 1024, 768, renderer);
         }
     }
 }
